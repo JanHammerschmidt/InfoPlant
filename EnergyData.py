9a7980b91a85da6a79f453ca099dd1e6a83fb06e
Jan Hammerschmidt <jhammers@techfak.uni-bielefeld.de>
2016-01-30 11:06:15 +0100

    + added (and testing) slow accumulated consumption

--- EnergyData.py
+++ EnergyData.py
@@ -4,13 +4,15 @@ import pandas as pd, numpy as np
 timedelta = pd.offsets.timedelta
 
 class CircleData(object):
-    def __init__(self, mac, fast_interval):
+    def __init__(self, mac, fast_interval, slow_interval):
         self.mac = mac
         self.log = pd.Series()
         self.slow_log = pd.Series()
         self.current_consumption = 0
         self.max_gap = fast_interval * 2.5
+        self.max_slow_gap = slow_interval * 1.1
         self.td8 = timedelta(seconds=8)
+        self.td10m = timedelta(minutes=10)
 
     def accumulated_consumption_fast(self, begin, end):
         if begin >= end:
@@ -51,6 +53,28 @@ class CircleData(object):
             acc += d[last] * dt
         return acc
 
+    def accumulated_consumption(self, begin, end):
+        # we disregard the first slow measurement after a gap here
+        if begin >= end:
+            return 0
+        d,index = self.slow_log, self.slow_log.index
+        first = bisect_left(index, begin)
+        last = bisect_right(index, end)
+        print(first,last)
+        if first == last or first >= len(index) or last == 0:
+            return self.accumulated_consumption_fast(begin, end)
+
+        acc = self.accumulated_consumption_fast(begin, index[first])
+        for i in range(first+1, last):
+            dt = (index[i] - index[i-1]).total_seconds()
+            if dt > self.max_slow_gap:
+                acc += self.accumulated_consumption_fast(index[i-1], index[i])
+            else:
+                acc += d[i]
+
+        return acc + self.accumulated_consumption_fast(index[last-1], end)
+
+
 class EnergyData(object):
     def __init__(self, log_path, slow_log_path, slow_interval=10*60, fast_interval=10):
         # self.log_path = log_path
@@ -69,25 +93,18 @@ class EnergyData(object):
             if not c.slow_log.index.sort_values().identical(c.slow_log.index):
                 print("slow-log-index not sorted")
                 c.slow_log.sort_index(inplace=True)
-
-        c = self.circles['8FB86B']
-        t = c.log.index
-        acc_all = c.accumulated_consumption_fast(t[0], t[-1])
-        acc = 0
-        t0 = t[0]
-        while True:
-            t1 = t0 + timedelta(seconds=10)
-            acc += c.accumulated_consumption_fast(t0, t1)
-            if t1 > t[-1]:
-                break
-            t0 = t1
-        print(acc_all)
-        print(acc)
-
-        c.accumulated_consumption_fast(t[0],t[-1])
-        c.accumulated_consumption_fast(t[0],t1)
-
-        pass
+            # c.accumulated_consumption(c.slow_log.index[-4] + pd.offsets.timedelta(seconds=1), c.slow_log.index[-3] - pd.offsets.timedelta(seconds=1)) #in-between
+            # c.accumulated_consumption(c.slow_log.index[0] - pd.offsets.timedelta(minutes=10), c.slow_log.index[0] - pd.offsets.timedelta(minutes=1)) # left
+            c.accumulated_consumption(c.slow_log.index[-2] - pd.offsets.timedelta(minutes=1), c.slow_log.index[-1] + pd.offsets.timedelta(minutes=10)) # right
+            c.accumulated_consumption(c.slow_log.index[5], c.slow_log.index[6]) # precisely on two log-points
+            c.accumulated_consumption(c.slow_log.index[5], c.slow_log.index[7]) # same, but further away: 3)
+            c.accumulated_consumption(c.slow_log.index[5], c.slow_log.index[5] + pd.offsets.timedelta(seconds=1)) # 4) end in-between (near)
+            c.accumulated_consumption(c.slow_log.index[5] + pd.offsets.timedelta(seconds=1), c.slow_log.index[6]) # 4) same, but the other way around
+            c.accumulated_consumption(c.slow_log.index[5], c.slow_log.index[7] + pd.offsets.timedelta(seconds=1)) # 4) same, but further away
+            c.accumulated_consumption(c.slow_log.index[0] - pd.offsets.timedelta(seconds=1), c.slow_log.index[0]) # 6)  left (end on log-point)
+            c.accumulated_consumption(c.slow_log.index[-1] - pd.offsets.timedelta(seconds=1), c.slow_log.index[-1]) # 7) right (end on log-point)
+            c.accumulated_consumption(c.slow_log.index[-2] - pd.offsets.timedelta(seconds=1), c.slow_log.index[-1]) # 7) same, but further away
+            c.accumulated_consumption(c.slow_log.index[-1] - pd.offsets.timedelta(seconds=3), c.slow_log.index[-1] - pd.offsets.timedelta(seconds=2)) # 7) in-between with gap
 
     def circle(self, mac):
         if len(mac) > 6:
@@ -95,7 +112,7 @@ class EnergyData(object):
         if mac in self.circles:
             return self.circles[mac]
         else:
-            c = CircleData(mac, self.fast_interval)
+            c = CircleData(mac, self.fast_interval, self.slow_interval)
             self.circles[mac] = c
             return c
 
